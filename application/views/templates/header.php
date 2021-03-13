c07d394f5ffa3f3f54c2cac7744e9764ee36a58e
Rananda Diki Pratama <39761611+ranandadiki@users.noreply.github.com>
2021-03-13 16:54:11 +0700

    Create header.php

---
+++ application/views/templates/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title><?php echo $title; ?></title>
+
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+	<style>
+		.container-fluid {
+			padding: 15px;
+		}
+
+		.control-label {
+			text-align: left !important;
+		}
+
+		.form-input {
+			max-width: 700px;
+			margin: 0 auto;
+		}
+
+		.btn-form {
+			text-align: right;
+		}
+	</style>
+</head>
+
+<body>
